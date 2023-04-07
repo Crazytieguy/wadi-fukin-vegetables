d85a0fcad1147030479b36a63de286dd61309eda
Yoav Tzfati <yoav.tzfati@gmail.com>
2023-04-08 02:53:27 +0300

    add order history

--- src/routes/order-history/+page.server.ts
+++ src/routes/order-history/+page.server.ts
@@ -1,3 +1,21 @@
+import { prisma } from '$lib/server/prismaClient';
+
 export const load = async ({ locals }) => {
-  return await locals.requireLogin();
+  const { user } = await locals.requireLogin();
+  const orders = prisma.order.findMany({
+    include: {
+      userOrderVegetables: true
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+  const vegetables = prisma.vegetable.findMany();
+  const users = prisma.user.findMany({
+    select: {
+      id: true,
+      name: true
+    }
+  });
+  return { user, orders, vegetables, users };
 };
